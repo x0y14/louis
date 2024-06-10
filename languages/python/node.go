080afbcf41ffe2366e9fc9cae6a26af1e10ea56a
x0y14 <ya01ma08@gmail.com>
2024-06-10 19:01:36 +0900

    nodeのアップデート

--- languages/python/node.go
+++ languages/python/node.go
@@ -1,8 +1,21 @@
 package python
 
+import "louis/interfaces"
+
+type NodeKind int
+
+func (n NodeKind) String() string {
+	return ""
+}
+
 type Node struct {
+	Kind NodeKind
 }
 
 func (n *Node) String() string {
 	return ""
+}
+
+func (n *Node) GetKind() interfaces.NodeKind {
+	return n.Kind
 }
