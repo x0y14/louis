45e2f91c13d76e47dfa75b2d74be3ff90cbc8d8d
x0y14 <ya01ma08@gmail.com>
2024-06-10 19:01:52 +0900

    fieldのついか

---
+++ languages/c/field.go
@@ -0,0 +1,22 @@
+package c
+
+type ReturnField struct {
+	Values *Node
+}
+
+type BlockField struct {
+	Stmts *Node
+}
+
+type VariableDeclField struct {
+}
+
+type VariableDefField struct {
+}
+
+type FunctionDefField struct {
+	ReturnType *Node
+	Ident      *Node
+	Params     *Node
+	Block      *Node
+}
